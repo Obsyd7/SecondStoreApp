37ccff9c848d4b9647965215991b7599d34a3315
Patryk Pater <paterp989@gmail.com>
2018-10-12 17:00:20 +0200

    ConnectionString&DbContext

---
+++ SecondStoreApp/DAL/StoreDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using SecondStoreApp.Models;
+
+namespace SecondStoreApp.DAL
+{
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext() : base("StoreDbContext")
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderPosition> OrderPositions { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
