71240fccc1d16265649f45152403a7fe0d068412
Patryk Pater <paterp989@gmail.com>
2018-10-12 19:20:22 +0200

    FirstCompileTest

--- SecondStoreApp/Controllers/HomeController.cs
+++ SecondStoreApp/Controllers/HomeController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SecondStoreApp.DAL;
+using SecondStoreApp.Models;
 
 namespace SecondStoreApp.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        private StoreDbContext db = new StoreDbContext();
+
         public ActionResult Index()
         {
+            Category category = new Category
+            {
+                CategoryName = "asp.net mvc",
+                IconFileName = "aspNetMvc.png",
+                CategoryDescription = "Description"
+            };
+
+            db.Categories.Add(category);
+            db.SaveChanges();
+
             return View();
         }
     }
