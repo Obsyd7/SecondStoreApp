fdefffbd7daf618c51ef6f40f60f15c808a0ab1e
Patryk Pater <paterp989@gmail.com>
2018-10-15 11:50:39 +0200

    AddCategoryMenuPartialView

--- SecondStoreApp/Controllers/CourseController.cs
+++ SecondStoreApp/Controllers/CourseController.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.UI.WebControls;
+using SecondStoreApp.DAL;
 
 namespace SecondStoreApp.Controllers
 {
     public class CourseController : Controller
     {
+        private StoreDbContext db = new StoreDbContext();
+
         public ActionResult Index()
         {
             return View();
@@ -22,6 +25,13 @@ namespace SecondStoreApp.Controllers
         public ActionResult Details(string id)
         {
             return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult CategoryMenu(string id)
+        {
+            var categories = db.Categories.ToList();
+            return PartialView("_CategoryMenu", categories);
         }
     }
 }
