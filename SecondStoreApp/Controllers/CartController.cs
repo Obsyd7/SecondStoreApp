052c480853542be883ec2f0a7e9ea37abb90db71
Patryk Pater <paterp989@gmail.com>
2018-10-29 11:54:42 +0100

    CartBackend + PartialViews (Pending)

--- SecondStoreApp/Controllers/CartController.cs
+++ SecondStoreApp/Controllers/CartController.cs
@@ -3,19 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SecondStoreApp.DAL;
+using SecondStoreApp.Infrastructure;
+using SecondStoreApp.ViewModels;
 
 namespace SecondStoreApp.Controllers
 {
     public class CartController : Controller
     {
-        public ActionResult Index()
+        private CartManager cartManager;
+
+        private SessionManager sessionManager { get; set; }
+
+        private StoreDbContext db;
+
+        public CartController()
         {
-            return View();
+            db = new StoreDbContext();
+            sessionManager = new SessionManager();
+            cartManager = new CartManager(sessionManager, db);
         }
 
-        public ActionResult AddToCart(string id)
+        public ActionResult Index()
         {
-            return View();
+            var orderItem = cartManager.DownloadCart();
+            var fullPrice = cartManager.DownloadCartValue();
+
+            var cartViewModel = new CartViewModel()
+            {
+                OrderPosition = orderItem,
+                TotalPrice = fullPrice
+            };
+
+            return View(cartViewModel);
         }
+
+        public ActionResult AddToCart(int id)
+        {
+            cartManager.AddToCart(id);
+            return RedirectToAction("Index");
+        }
+
+        public int DownloadNumberOfItemsFromCart()
+        {
+            return cartManager.DownloadCartItemNumber();
+        }
+
+
     }
 }
---
+++ SecondStoreApp/ViewModels/CartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SecondStoreApp.Models;
+
+namespace SecondStoreApp.ViewModels
+{
+    public class CartViewModel
+    {
+        public List<CartItem> OrderPosition { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
